1e3379920f352bb6fb29d88f21566a4e9ee721ba
kainy ryu <kainy@joara.com>
2021-12-05 16:19:48 +0900

    Start from srcatch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import styled from '@emotion/styled';
-import Header from './components/Header';
-import Menu from './components/Menu';
-import Home from './pages/Home';
 
 export default function App() {
   return (
     <Router>
-      <BG className="App">
-        <Header />
-        <Routes>
-          <Route path="/about" />
-          <Route path="/program" />
-          <Route path="/community" />
-          <Route path="/qna" />
-          <Route path="/map" />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BG>
+      <BG className="App"></BG>
     </Router>
   );
 }
